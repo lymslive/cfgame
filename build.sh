861e11a2ade555c5dddc210d4bcfb53b5638cc7f
lymslive <403708621@qq.com>
2018-05-03 11:50:36 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,16 @@
+#! /bin/env bash
+
+# 生成程序名同项目名，即父目录名
+cwd=$(pwd)
+project=$(basename $cwd)
+echo $cwd $project
+
+# 备份上次生成的程序
+if [[ -x $project ]]; then
+	echo "bakup executable $project"
+	mv $project $project.bak
+fi
+
+# 在当前目录生成程序
+echo "go build new executable $project"
+go build
